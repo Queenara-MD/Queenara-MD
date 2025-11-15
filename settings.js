7d85f84920ec9f2e41ff6b96640b8b24cf142d85
MQueenara-MD <yousilegend@gmail.com>
2025-11-15 10:46:12 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Master Stormy',
+  author: '‎',
+  botName: "Queenara-MD",
+  botOwner: 'Master Stormy', // Your name
+  ownerNumber: '923109506687', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.2",
+  updateZipUrl: "https://github.com/Queenara-MD/Queenara-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
